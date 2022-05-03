7f397f65d02ff8a1a543dedbf98f4388e6b906fc
AvrahamMicael <avrahammicael@protonmail.com>
2022-05-03 17:27:18 -0300

    last chapter uploaded

--- resources/views/manga/chapter.blade.php
+++ resources/views/manga/chapter.blade.php
@@ -9,9 +9,13 @@
         <img src="{{ asset($page->path) }}" alt="page-{{ $page->order }}" class="img-fluid mb-3"><br>
         @include('manga._partials.pagination')
 
-        @if ($page->order == $manga->pages_count && ($chapter_order + 1) <= $manga->chapters_count)
+        @if ($page->order == $manga->pages_count)
             <div class="d-flex justify-content-center p-2">
-                <a href="{{ route('app.manga.view', ['id' => $manga->id, 'chapter_order' => $chapter_order+1]) }}" class="btn btn-light btn-lg">Go to Next Chapter</a>
+                @if ($chapter_order + 1 <= $manga->chapters_count)
+                    <a href="{{ route('app.manga.view', ['id' => $manga->id, 'chapter_order' => $chapter_order+1]) }}" class="btn btn-light btn-lg">Go to Next Chapter</a>
+                @else
+                    <a href="{{ route('app.manga.main', $manga->id) }}" class="btn btn-light btn-lg">Last Uploaded Chapter</a>
+                @endif
             </div>
         @endif
     </div>
