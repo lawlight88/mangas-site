7d2ab9cd515b524b1f6057daea52eff7a96fc797
AvrahamMicael <avrahammicael@protonmail.com>
2022-05-16 18:40:11 -0300

    admin all scans pagination

--- resources/views/all_scans.blade.php
+++ resources/views/all_scans.blade.php
@@ -4,32 +4,6 @@
 
 @section('content')
 
-    @foreach ($scans as $key => $scan)
-        @if ($key % 4 == 0)
-            <div class="row">
-        @endif
-        <div class="col-md-3">
-            <div class="bg-dark-1 mb-4 p-3">
-                <a href="{{ route('app.scan.view', $scan) }}">
-                    <img class="img-fluid mx-auto d-block" src="{{ asset($scan->image) }}">
-                </a>
-                <div class="py-3">
-                    <div>
-                        {{$scan->name}}
-                    </div>
-                    <div>
-                        <small>Created At: {{$scan->created_at->format('Y-m-d')}}</small>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @if (($key + 1) % 4 == 0)
-            </div>
-        @endif
-    @endforeach
-
-    <div class="d-flex justify-content-center">
-        {{ $scans->links() }}
-    </div>
+    @include('includes.all_scans')
 
 @endsection
---
+++ resources/views/includes/all_scans.blade.php
@@ -0,0 +1,27 @@
+@foreach ($scans as $key => $scan)
+    @if ($key % 4 == 0)
+        <div class="row">
+    @endif
+    <div class="col-md-3">
+        <div class="bg-dark-1 mb-4 p-3">
+            <a href="{{ Request::routeIs('scan.all') ? route('scan.view', $scan->id) : route('app.scan.view', $scan) }}">
+                <img class="img-fluid mx-auto d-block" src="{{ asset($scan->image) }}">
+            </a>
+            <div class="py-3">
+                <div>
+                    {{$scan->name}}
+                </div>
+                <div>
+                    <small>Created At: {{$scan->created_at->format('Y-m-d')}}</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    @if (($key + 1) % 4 == 0)
+        </div>
+    @endif
+@endforeach
+
+<div class="d-flex justify-content-center">
+    {{ $scans->links() }}
+</div>
